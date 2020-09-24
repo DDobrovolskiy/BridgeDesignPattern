b5ea7b259d43f85429ac57615f329d4cfcbe8b6a
DmitriyDobrovolskiy <dda.90@mail.ru>
2020-09-24 08:51:40 +0500

    Begin
    Create pattern console

---
+++ BridgeDesignPattern/BridgeDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BridgeDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
